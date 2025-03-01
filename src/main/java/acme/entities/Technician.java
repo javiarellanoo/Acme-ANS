df95faeb894414cb0eec23be545a0c8444510809
aaronacuuna <aaronacuna451@gmail.com>
2025-03-01 13:50:35 +0100

    feat: add Technician entity
    // TODO: ask for the maximum number of years of experience in the forum

---
+++ src/main/java/acme/entities/Technician.java
@@ -0,0 +1,58 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Technician extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				licenseNumber;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				specialisation;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				annualHealthTest;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 35) // TODO: ask for the maximum number of years of experience in the forum
+	@Automapped
+	private Integer				experienceYears;
+
+	@Optional
+	@ValidString
+	@Automapped
+	private String				certifications;
+
+}
