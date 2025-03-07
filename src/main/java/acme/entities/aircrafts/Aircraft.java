b36966a769c9701207d9606de59ddacdcd5ee013
CRISTIAN COSMA <ionlac@alum.us.es>
2025-03-07 13:51:32 +0100

    feat: Aircraft entity added

---
+++ src/main/java/acme/entities/aircrafts/Aircraft.java
@@ -0,0 +1,51 @@
+
+package acme.entities.aircrafts;
+
+import javax.persistence.Column;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+public class Aircraft extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50, min = 1)
+	@Automapped
+	private String				model;
+
+	@Mandatory
+	@ValidString(max = 50, min = 1)
+	@Column(unique = true)
+	private String				registrationNumber;
+
+	@Mandatory
+	@ValidNumber(max = 255, min = 1)
+	@Automapped
+	private Integer				capacity;
+
+	@Mandatory
+	@ValidNumber(max = 50000, min = 2000)
+	@Automapped
+	private Integer				cargoWeight;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private AircraftStatus				status;
+
+	@Optional
+	@ValidString(max = 255, min = 0)
+	@Automapped
+	private String				details;
+}
