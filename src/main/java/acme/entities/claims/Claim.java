0778cd2f92f9f647706438dbbb5ecb1c8250d400
mpradoj04 <mprajim860@gmail.com>
2025-03-07 11:19:02 +0100

    feat: Create claim entity

---
+++ src/main/java/acme/entities/claims/Claim.java
@@ -0,0 +1,64 @@
+
+package acme.entities.claims;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.assistanceAgents.AssistanceAgent;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				passengerEmail;
+
+	@Optional
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private ClaimType			type;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				accepted;
+
+	// Relationships
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private AssistanceAgent		registeredBy;
+
+}
