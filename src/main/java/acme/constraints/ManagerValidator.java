e47ff8df1d998da5ade209882a2e9253cbbac648
Javier Arellano <javarelop@alum.us.es>
2025-03-07 14:35:38 +0100

    feat: Added Manager role

---
+++ src/main/java/acme/constraints/ManagerValidator.java
@@ -0,0 +1,51 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.principals.DefaultUserIdentity;
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.realms.Manager;
+
+@Validator
+public class ManagerValidator extends AbstractValidator<ValidManager, Manager> {
+
+	@Override
+	protected void initialise(final ValidManager annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final Manager manager, final ConstraintValidatorContext context) {
+		boolean result;
+
+		assert context != null;
+
+		if (manager == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+			Boolean matches;
+			String initials;
+			String[] surnames;
+			DefaultUserIdentity identity;
+
+			identity = manager.getIdentity();
+
+			surnames = identity.getSurname().split(" ");
+
+			initials = "";
+
+			initials += identity.getName().trim().charAt(0);
+
+			for (String surname : surnames)
+				initials += surname.trim().charAt(0);
+
+			matches = manager.getIdentifier().trim().startsWith(initials);
+			super.state(context, !matches, "identifier", "acme.validation.manager.identifier.message");
+		}
+		result = !super.hasErrors(context);
+		return result;
+	}
+
+}
