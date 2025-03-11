1bfbf7d656bea43862c241725a50f0094937efbf
mpradoj04 <mprajim860@gmail.com>
2025-03-11 12:52:35 +0100

    feat: Implement tracking logs

---
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -0,0 +1,47 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.entities.trackingLogs.TrackingLog;
+import acme.entities.trackingLogs.TrackingLogStatus;
+
+public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, TrackingLog> {
+
+	// ConstraintValidator interface
+
+	@Override
+	protected void initialise(final ValidTrackingLog annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final TrackingLog trackingLog, final ConstraintValidatorContext context) {
+		Boolean result;
+		assert context != null;
+
+		if (trackingLog == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+
+			Boolean correctStatus;
+			Boolean hasResolution;
+
+			if (trackingLog.getResolutionPercentage() == 100.00) {
+				correctStatus = trackingLog.getStatus().equals(TrackingLogStatus.ACCEPTED) || trackingLog.getStatus().equals(TrackingLogStatus.REJECTED);
+				hasResolution = trackingLog.getResolution() != null && !trackingLog.getResolution().isEmpty();
+			} else
+				correctStatus = trackingLog.getStatus().equals(TrackingLogStatus.PENDING);
+
+			super.state(context, !correctStatus, "status", "acme.validation.trackingLog.status.message");
+			super.state(context, !hasResolution, "resolution", "acme.validation.trackingLog.resolution.message");
+
+		}
+
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+
+}
