95f90e1873b4ae751a67140097fe4011ef8c9df3
CRISTIAN COSMA <ionlac@alum.us.es>
2025-03-11 15:46:06 +0100

    feat: added passenger entity

---
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -0,0 +1,63 @@
+
+package acme.entities.passenger;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.bookings.Booking;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Passenger extends AbstractEntity {
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 255, min = 1)
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthDate;
+
+	@Optional
+	@ValidString(max = 50, min = 0)
+	@Automapped
+	private String				specialNeeds;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Booking				booking;
+
+}
