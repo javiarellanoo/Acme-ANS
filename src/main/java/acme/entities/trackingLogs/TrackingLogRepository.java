9cc092076ccee81f15813a6e922afb3ff7833ca2
mpradoj04 <mprajim860@gmail.com>
2025-03-12 10:38:43 +0100

    fix: Improved tracking logs custom validator

--- src/main/java/acme/entities/trackingLogs/TrackingLogRepository.java
+++ src/main/java/acme/entities/trackingLogs/TrackingLogRepository.java
@@ -1,6 +1,8 @@
 
 package acme.entities.trackingLogs;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface TrackingLogRepository extends AbstractRepository {
 
-	@Query("select max(t.resolutionPercentage) from TrackingLog t")
-	Double getLastTrakingLogPercentage();
+	@Query("SELECT t FROM TrackingLog t ORDER BY t.trackIndex ASC")
+	List<TrackingLog> findAllOrderedByIndex();
 
 }
