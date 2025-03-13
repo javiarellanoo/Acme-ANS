8b107595a50698711a7954c6b5efc80e540f6f98
mpradoj04 <mprajim860@gmail.com>
2025-03-13 12:51:24 +0100

    fix: changes in properties Student#4

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -59,6 +59,6 @@ public class Claim extends AbstractEntity {
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private AssistanceAgent		registeredBy;
+	private AssistanceAgent		assistanceAgent;
 
 }
