7369a62b6014eab6aa0a51755e27f5a21b07859f
aaronacuuna <aaronacuna451@gmail.com>
2025-03-25 16:47:47 +0100

    fix: add uniqueness constraint to technicians

--- src/main/java/acme/constraints/TechnicianValidator.java
+++ src/main/java/acme/constraints/TechnicianValidator.java
@@ -3,13 +3,20 @@ package acme.constraints;
 
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.components.principals.DefaultUserIdentity;
 import acme.client.components.validation.AbstractValidator;
 import acme.client.components.validation.Validator;
 import acme.realms.Technician;
+import acme.realms.repositories.TechnicianRepository;
 
 @Validator
 public class TechnicianValidator extends AbstractValidator<ValidTechnician, Technician> {
+
+	@Autowired
+	TechnicianRepository technicianRepository;
+
 
 	@Override
 	protected void initialise(final ValidTechnician annotation) {
@@ -26,6 +33,7 @@ public class TechnicianValidator extends AbstractValidator<ValidTechnician, Tech
 			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
 		else {
 			Boolean matches;
+			Boolean unique;
 			String initials;
 			DefaultUserIdentity identity;
 
@@ -38,7 +46,14 @@ public class TechnicianValidator extends AbstractValidator<ValidTechnician, Tech
 			initials += identity.getSurname().trim().charAt(0);
 
 			matches = technician.getLicenseNumber().trim().startsWith(initials);
-			super.state(context, matches, "licenseNumber", "acme.validation.technician.licenseNumber.message");
+
+			Technician alreadyExistingTechnician = this.technicianRepository.findTechnicianByLicenseNumber(technician.getLicenseNumber());
+
+			unique = alreadyExistingTechnician == null || alreadyExistingTechnician.getId() == technician.getId();
+
+			boolean condition = matches && unique;
+
+			super.state(context, condition, "licenseNumber", "acme.validation.technician.licenseNumber.message");
 		}
 
 		result = !super.hasErrors(context);
