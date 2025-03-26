7481dfb59252f94f6c57a45d17446927ff9ec405
CRISTIAN COSMA <ionlac@alum.us.es>
2025-03-26 17:57:07 +0100

    fix: correct query syntax in BookingRepository for locatorCode

--- src/main/java/acme/entities/bookings/BookingRepository.java
+++ src/main/java/acme/entities/bookings/BookingRepository.java
@@ -9,7 +9,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface BookingRepository extends AbstractRepository {
 
-	@Query("select b from Booking b where where locatorCode = :locatorCode")
+	@Query("select b from Booking b where b.locatorCode = :locatorCode")
 	Booking getBookingByLocatorCode(String locatorCode);
 
 	@Query("select count(b) from Booking b where locatorCode = :locatorCode")
