94e2e8d6ae0dd03b886309ec6d6b7e2cfa4c006e
Javier Arellano <javarelop@alum.us.es>
2025-04-17 21:49:21 +0200

    fix: Minor fix regarding validation message

--- src/main/java/acme/features/manager/flight/ManagerFlightCreateService.java
+++ src/main/java/acme/features/manager/flight/ManagerFlightCreateService.java
@@ -44,7 +44,7 @@ public class ManagerFlightCreateService extends AbstractGuiService<Manager, Flig
 		int airlineId;
 		Airline airline;
 
-		airlineId = super.getRequest().getData("airlines", int.class);
+		airlineId = super.getRequest().getData("airline", int.class);
 		airline = this.repository.findAirlineById(airlineId);
 
 		super.bindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
