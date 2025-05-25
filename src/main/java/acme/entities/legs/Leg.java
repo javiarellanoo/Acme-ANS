5a30dc0e3eebd1de8492592c697fec82fbd441ad
CRISTIAN COSMA <ionlac@alum.us.es>
2025-05-25 13:43:00 +0200

    Update booking publish and update test cases with new locator codes and error messages

--- src/main/java/acme/entities/legs/Leg.java
+++ src/main/java/acme/entities/legs/Leg.java
@@ -6,7 +6,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -29,6 +31,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidLeg
+@Table(indexes = {
+	@Index(columnList = "scheduledDeparture, flight_id"), @Index(columnList = "scheduledArrival, flight_id")
+})
 public class Leg extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -4,7 +4,9 @@ package acme.entities.passenger;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "customer_id"), @Index(columnList = "draftMode"), @Index(columnList = "customer_id, draftMode")
+})
 public class Passenger extends AbstractEntity {
 	// Serialisation version --------------------------------------------------
 
